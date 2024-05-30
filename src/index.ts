ce198c79b2953d50d5d4b59bd453be92d25384d1
Carlos Daniel <carlosporrasayala@gmail.com>
2024-05-30 17:54:36 -0500

    Conectar con base de datos de mysql

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import app from './app';
+import { AppDataSource } from './db/conexion';
 
-function main() {
-    app.listen(3500, () => {
-        console.log("Servidor activo");
-    })
+async function main() {
+    try {
+        await AppDataSource.initialize().then(() => {
+            console.log('Database');
+        })
+        app.listen(3500, () => {
+            console.log("Servidor activo");
+        })
+    } catch (err) {
+        if (err instanceof Error) {
+            console.log(err.message);
+        }
+    }
 }
 
 main()
